5b12bd16945ca26492d00ee7221927c3eb15114c
Sungmin Kim <seongmin.kim082@gmail.com>
2023-05-01 20:10:17 -0400

    fixed font size

--- app/ozen-sushi/page.tsx
+++ app/ozen-sushi/page.tsx
@@ -92,13 +92,14 @@ function OzenSushiHome() {
           </div>
         </div>
       </section>
-      <section className="relative w-full h-full">
+      <section className="relative w-full min-h-full">
         <Image
           alt="image_7626"
           src={image_7626}
-          className="absolute min-w-full min-h-full -z-10"
+          // height={400}
+          className="absolute min-w-full h-full -z-10"
         />
-        <div className="flex flex-col items-end text-end pr-40 mlg:pr-20 py-32 mlg:py-24">
+        <div className="flex flex-col items-end text-end pr-56 py-32 lg:pr-20 lg:py-24">
           <span className="monserratBold900 text-5xl mlg:text-4xl text-white mb-2">
             Get 10% Off!
           </span>
@@ -106,7 +107,7 @@ function OzenSushiHome() {
             Online Order!
           </span>
           <div className="border-2 w-[3.75rem] border-white mb-5" />
-          <div className="text-white text-xl lg:text-lg w-[40rem] mb-6">
+          <div className="text-white text-xl w-[40rem] mb-6 lg:text-lg mlg:text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             consectetur augue sit amet viverra varius. Suspendisse porttitor
             placerat enim nec molestie. Nullam dolor nisi, dignissim quis sapien
