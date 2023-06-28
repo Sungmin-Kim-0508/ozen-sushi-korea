15a2e2240caa055c67e86e0bbf5ad6312e755247
Sungmin Kim <seongmin.kim082@gmail.com>
2023-06-28 05:44:48 -0400

    added web icon

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,8 @@
 export default function Head() {
   return (
     <head>
-      <title>Main Page</title>
+      <title>Ozen London</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
--- app/ozen-sushi/head.tsx
+++ app/ozen-sushi/head.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 export default function Head() {
   return (
     <>
       <title>Ozen Sushi</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/ozen_sushi_icon.ico" />
     </>
   );
 }
